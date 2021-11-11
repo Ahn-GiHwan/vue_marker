010340e402e087052f2d42692b248c13b7bc96ba
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-11 23:43:00 +0900

    feat: user store module

---
+++ src/store/user.js
@@ -0,0 +1,18 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    currentUser: null
+  }),
+  mutations: {
+    assignState(state, payload) {
+      Object.keys(payload).forEach((key) => {
+        state[key] = payload[key]
+      })
+    }
+  },
+  actions: {
+    logout({ commit }) {
+      commit('assignState', { currentUser: null })
+    }
+  }
+}
