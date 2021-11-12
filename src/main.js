580407bbe975ea039ca51ed3acf9eecdc973db42
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-12 16:58:51 +0900

    feat: import and use, total component setting
    import
    - vuesweetalert2
    - plugins
    - styles
    
    use
    - plugins
    - router
    - store
    - total component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,38 @@
 import { createApp } from 'vue'
+import VueSweetalert2 from 'vue-sweetalert2'
+
+import '~/routes/guards'
 import App from '~/App.vue'
+import Loader from '~/components/Loader'
 import router from '~/routes'
 import store from '~/store'
+import { user, account, goods, goodsMaster } from '~/plugins'
 
-const app = createApp(App)
-app.use(router)
-app.use(store)
-app.mount('#app')
+import './style/style.css'
+import 'sweetalert2/dist/sweetalert2.min.css'
+
+const endpoint = 'https://asia-northeast3-heropy-api.cloudfunctions.net/api'
+
+const headers =  {
+    'content-type': 'application/json',
+    'apikey': process.env.apiKey,
+    'username': process.env.username,
+  }
+
+const adminHeader = {
+  'content-type': 'application/json',
+  'apikey': process.env.apiKey,
+  'username': process.env.username,
+  masterKey : process.env.masterKey
+}
+
+createApp(App)
+  .use(user, {endpoint, headers})
+  .use(account, {endpoint, headers})
+  .use(goods, {endpoint, headers})
+  .use(goodsMaster, {endpoint, headers, adminHeader})
+  .use(router)
+  .use(store)
+  .use(VueSweetalert2)
+  .component('Loader', Loader)
+  .mount('#app')
