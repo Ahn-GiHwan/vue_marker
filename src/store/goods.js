12aff20527e7e60b82c520b99f4a25e7bb1a1b6b
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-13 00:28:25 +0900

    feat: create mutations, actions of goods store

---
+++ src/store/goods.js
@@ -0,0 +1,18 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    goodsList: []
+  }),
+  mutations: {
+    assignState(state, payload) {
+      Object.keys(payload).forEach((key) => {
+        state[key] = payload[key]
+      })
+    }
+  },
+  actions: {
+    searchGoodsList({ commit }, goodsList) {
+      commit('assignState', { goodsList })
+    }
+  }
+}
