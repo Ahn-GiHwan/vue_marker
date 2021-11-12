e42112946d71047fb865d490499863e50608cc73
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-13 00:26:32 +0900

    feat: tbody part of GoodsList Table

---
+++ src/routes/admin/GoodsListTable.vue
@@ -0,0 +1,22 @@
+<template>
+  <tbody>
+    <tr>
+      <td>{{ goods.title }}</td>
+      <td>{{ goods.price.toLocaleString()+'원' }}</td>
+      <td>{{ goods.isSoldOut }}</td>
+      <td><button>상세</button></td>
+      <td><button>수정</button></td>
+    </tr>
+  </tbody>
+</template>
+
+<script>
+export default {
+  props: {
+    goods: {
+      type: Object,
+      default: ()=>({})
+    }
+  }
+}
+</script>
