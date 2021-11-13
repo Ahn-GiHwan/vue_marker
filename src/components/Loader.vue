3a84e5ca4ca0d48f73a754979562698f7a3bdbcb
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-13 18:26:48 +0900

    feat: loading animation component

---
+++ src/components/Loader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    v-show="loading"
+    :style="{
+      width: `${size}px`,
+      height: `${size}px`,
+      borderWidth: `${width}px`,
+      borderColor: color
+    }"
+    class="loader">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    size:{
+      type: Number,
+      default: 30
+    },
+    color:{
+      type:String,
+      default: '#1976D2'
+    },
+    width: {
+      type: Number,
+      default: 4
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
