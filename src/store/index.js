273f9c6a659aee79ef8b042779cabf16795e91fa
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-14 01:09:36 +0900

    feat: alert store import module

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { createStore } from 'vuex'
 import user from './user'
 import account from './account'
 import goods from './goods'
+import alert from './alert'
 
 export default createStore({
   modules: {
     user,
     account,
-    goods
+    goods,
+    alert
   },
 })
