ad1883a08f5d37cc0cdf39d55a11ff029600f45c
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-13 18:36:22 +0900

    feat: 내 정보 수정을 위한 navigation component

---
+++ src/routes/user/update/UpdateUser.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2 class="text-2xl p-5">
+    내 정보 수정
+  </h2>
+  <section class="w-full">
+    <div class="flex justify-around items-center w-full h-12 bg-blue-50">
+      <RouterLink
+        to="displayname"
+        class="border-2 border-solid rounded-md p-1 hover:text-gray-400">
+        닉네임
+      </RouterLink>
+      <RouterLink
+        to="password"
+        class="border-2 border-solid rounded-md p-1 hover:text-gray-400">
+        비밀번호 변경
+      </RouterLink>
+      <RouterLink
+        to="profileimg"
+        class="border-2 border-solid rounded-md p-1 hover:text-gray-400">
+        이미지 변경
+      </RouterLink>
+    </div>
+    <RouterView />
+  </section>
+</template>
