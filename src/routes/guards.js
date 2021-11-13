fb18c7fefc94f44fc09d83d857326f81c196d6bf
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-13 18:30:21 +0900

    feat: 경우에 따라 경로 제한

--- src/routes/guards.js
+++ src/routes/guards.js
@@ -1,20 +1,55 @@
+import axios from 'axios'
 import auth from '~/utils/auth'
 import router from './index'
 import store from '~/store'
 
-router.beforeEach(async to => {
-  // console.log(to)
-  
-  try {
-    const user = await auth()
+router.beforeEach(async (to) => {
+  const getStorage = localStorage.getItem('token')
+  const { currentUser } = store.state.user
+  const { update } = store.state.account
 
-    store.commit('user/assignState', {
-      currentUser: user
-    })
-  } catch (error) {
-    const title = error.response.data
-    // alert(title)
+  if (getStorage) {
+    if (!currentUser) {
+      console.log('guard getUser')
+      console.log('12321312')
+      const user = await auth()
+
+      store.commit('user/assignState', {
+        currentUser: user
+      })
+      const { currentUser } = store.state.user
+      if (currentUser.email !== process.env.admin && to.href.includes('admin')) {
+        router.push('/about')
+      }
+    }
+    if (!update) {
+      console.log('guard getAccount')
+      const { totalBalance, accounts } = await myAccountInfo()
+      store.commit('account/assignState', {
+        totalBalance, accounts, update: true
+      })
+    }
+  } else {
+    console.log('guard isLogin')
+    if (to.href.includes('about') || to.href.includes('admin')) {
+      router.push('/')
+    }
   }
 })
+
+async function myAccountInfo () {
+  const { data } = await axios({
+    url: 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/account',
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json',
+      'apikey': process.env.apiKey,
+      'username': process.env.username, 
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    },
+  })
+  console.log('myAccountInfo', data)
+  return data
+}
 
 
