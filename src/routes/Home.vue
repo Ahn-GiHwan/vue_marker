d6a19385274684311705cc665863ec1769438c88
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-13 18:28:35 +0900

    feat: add InputBox&GoodsList component

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,14 +1,16 @@
 <template>
-  <h1>Home page..</h1>
-  <h2>{{ heropyMessage }}</h2>
+  <InputBox ref="title" />
+  <GoodsList />
 </template>
 
 <script>
+import InputBox from '~/components/InputBox'
+import GoodsList from '~/components/GoodsList'
+
 export default {
-  computed: {
-    heropyMessage() {
-      return this.$store.state.heropy.message
-    }
-  }
+  components: {
+    InputBox,
+    GoodsList
+  },
 }
 </script>
