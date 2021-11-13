a1ba61babcab66abab2fac8095fc82b22d4558bf
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-13 18:29:57 +0900

    feat: add router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from './Home'
-import About from './About'
 import LogIn from './LogIn'
 import SignUp from './SignUp'
+import Home from './Home'
+
+import About from './user/About.vue'
+import UpdateUser from './user/update/UpdateUser'
+import DisplayName from './user/update/DisplayName'
+import Password from './user/update/Password'
+import ProfileImg from './user/update/ProfileImg'
+import MyAccount from './user/MyAccount'
+import AddAccount from './user/AddAccount'
+import MySellList from './user/MySellList'
+
+import GoodsInfo from './goods/GoodsInfo'
+
+import Admin from './admin/Admin'
+import GoodsList from './admin/GoodsList'
+import SellList from './admin/SellList'
+import AddGoods from './admin/AddGoods'
+import AdminGoodsInfo from './admin/GoodsUpdate.vue'
+
+import InputAccount from './account/InputAccount'
 
 export default createRouter({
   history: createWebHistory(),
@@ -14,7 +32,65 @@ export default createRouter({
     },
     {
       path: '/about',
-      component: About
+      component: About,
+      redirect: { name: 'update' },
+      children: [
+        {
+          path: 'update',
+          name: 'update',
+          component: UpdateUser,
+          redirect: { name: 'displayname' },
+          children: [
+            { path: 'displayname', name: 'displayname', component: DisplayName },
+            { path: 'password', component: Password },
+            { path: 'profileimg', component: ProfileImg },
+          ]
+        },
+        {
+          path: 'myaccount',
+          component: MyAccount,
+          children: [
+            {
+              path: 'addaccount',
+              component: AddAccount,
+            },
+            {
+              path: 'inputaccount',
+              name: 'inputaccount',
+              component: InputAccount
+            }
+          ]
+        },
+        {
+          path: 'myselllist',
+          component: MySellList
+        }
+      ]
+    },
+    {
+      path: '/admin',
+      component: Admin,
+      redirect: { name: 'goodslist' },
+      children: [
+        {
+          path: 'goodslist',
+          name: 'goodslist',
+          component: GoodsList,
+        },
+        {
+          path: 'selllist',
+          component: SellList
+        },
+        {
+          path: 'addGoods',
+          component: AddGoods
+        },
+        {
+          path: '/admin/goods/:id',
+          name: 'admingoodsinfo',
+          component: AdminGoodsInfo
+        },
+      ]
     },
     {
       path: '/login',
@@ -23,6 +99,11 @@ export default createRouter({
     {
       path: '/signup',
       component: SignUp
-    }
+    },
+    {
+      path: '/goods/:id',
+      name: 'goodsInfo',
+      component: GoodsInfo
+    },
   ]
 })
