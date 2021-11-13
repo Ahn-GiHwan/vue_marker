517383fc46bdd5300900b4a92821f2c7118bd2a1
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-13 18:33:29 +0900

    feat: 관리자 페이지의 navigation bar component

---
+++ src/routes/admin/Admin.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="flex justify-around items-center h-14 bg-blue-100">
+    <RouterLink
+      to="/admin/goodslist"
+      class="border-2 border-solid rounded-md p-1 hover:text-gray-400">
+      전체 제품 보기
+    </RouterLink>
+    <RouterLink
+      to="/admin/selllist"
+      class="border-2 border-solid rounded-md p-1 hover:text-gray-400">
+      전체 판매 내역 보기
+    </RouterLink>
+    <RouterLink
+      to="/admin/addGoods"
+      class="border-2 border-solid rounded-md p-1 hover:text-gray-400">
+      제품 추가 하기
+    </RouterLink>
+  </section>
+  <RouterView />
+</template>
+
+<script>
+export default {
+
+}
+</script>
