57e7ba92d1eba7b721200eb7a3812f80ff70753d
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-14 00:33:01 +0900

    feat: add Alert component funciton

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <InputBox />
   <GoodsList />
+  <Alert
+    :show="show"
+    :title="title"
+    :content="content"
+    :path="path" />
 </template>
 
 <script>
@@ -12,5 +17,26 @@ export default {
     InputBox,
     GoodsList
   },
+  data(){
+    return {
+      show: false,
+      title: '',
+      content: '',
+      path: ''
+    }
+  },
+  mounted(){
+    if(this.$store.state.user.currentUser) {
+      if(!this.$store.state.account.accounts.length) {
+        this.show = true
+        this.title = '현재 등록된 계좌가 없습니다!'
+        this.content = '계좌를 등록하러 가시겠습니까?'
+        this.path = '/about/myaccount/addaccount'
+        setTimeout(() => {
+          this.show = false
+        }, 5000)
+      }
+    }
+  }
 }
 </script>
