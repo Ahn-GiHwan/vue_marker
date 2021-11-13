cb6f96701f102e0e0d17d41749a0d43bf5865333
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-14 01:10:10 +0900

    feat: create Alert component

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,9 +1,47 @@
 <template>
-  <h1>alert</h1>
+  <section
+    v-show="show"
+    class="alert absolute right-2 bottom-2">
+    <div class="relative flex flex-col justify-center items-center rounded-xl p-5 m-5 bg-blue-400 text-white">
+      <span
+        v-if="title"
+        class="text-lg font-bold">{{ title }}</span>
+      <span v-if="text">{{ text }}</span>
+      <button
+        v-if="path"
+        class="border border-solid border-white rounded-lg mt-2 px-1 bg-white text-black cursor-pointer"
+        @click="$router.push(path)">
+        이동
+      </button>
+      <button
+        class="absolute top-0 right-2"
+        @click="close">
+        x
+      </button>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    show(){
+      return this.$store.state.alert.show
+    },
+    title(){
+      return this.$store.state.alert.title
+    },
+    text(){
+      return this.$store.state.alert.text
+    },
+    path(){
+      return this.$store.state.alert.path
+    },
+  },
+  methods: {
+    close(){
+      this.$closeAlert()
+    }
+  }
 }
 </script>
