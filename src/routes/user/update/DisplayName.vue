83dc615089673d095a5cf9793e69b60caf2cd054
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-13 18:40:23 +0900

    feat: 닉네임 변경을 위한 component

---
+++ src/routes/user/update/DisplayName.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="w-full">
+    <h3 class="text-2xl p-5 text-center">
+      닉네임 수정
+    </h3>
+    <div class="flex justify-center items-center w-full">
+      <span>닉네임</span>
+      <input
+        ref="displayName"
+        :value="currentUser.displayName"
+        :placeholder="currentUser.displayName"
+        class="border-b-2 border-solid outline-none pl-1"
+        type="text" />
+      <button
+        class="border border-solid rounded-md p-2 bg-blue-300 text-white outline-none m-5"
+        @click="onSubmit">
+        수정하기
+      </button>
+      <Loader :loading="loading" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      loading: false,
+    }
+  },
+  computed: {
+    currentUser(){
+      return this.$store.state.user.currentUser
+    }
+  },
+  methods: {
+    validation(){
+      const { displayName } = this.$refs
+      if(!displayName.value){
+        this.$swal.fire({
+          title: '닉네임을 적어주세요',
+          icon: 'warning'
+        })
+        return false
+      }
+      return true
+    },
+    async onSubmit(){
+      try {
+        if(!this.validation()) return
+        if(this.loader) return
+        this.loader = true
+
+        const displayName = this.$refs.displayName.value
+        const currentUser = await this.$userPut({
+          displayName
+        })
+
+        this.$store.dispatch('user/update', currentUser)
+
+        this.$swal.fire({
+          title: '수정 완료!',
+          icon: 'success'
+        })
+      } catch (error) {
+        const title = error.response.data
+        this.$swal.fire({ title, icon: 'error' })
+      } finally {
+        this.loader = false
+      }
+    }
+  }
+}
+</script>
