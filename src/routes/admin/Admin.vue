40b5a6e97ee8ca7de6cb0bc06715764e042ec395
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-14 16:57:27 +0900

    style: add border color

--- src/routes/admin/Admin.vue
+++ src/routes/admin/Admin.vue
@@ -2,17 +2,17 @@
   <section class="sticky top-10 right-0 left-0 flex justify-around items-center h-14 bg-blue-100 z-10">
     <RouterLink
       to="/admin/goodslist"
-      class="border-2 border-solid rounded-md p-1 hover:text-gray-400">
+      class="border-2 border-solid rounded-md border-white p-1 hover:text-gray-400">
       전체 제품 보기
     </RouterLink>
     <RouterLink
       to="/admin/selllist"
-      class="border-2 border-solid rounded-md p-1 hover:text-gray-400">
+      class="border-2 border-solid rounded-md border-white p-1 hover:text-gray-400">
       전체 판매 내역 보기
     </RouterLink>
     <RouterLink
       to="/admin/addGoods"
-      class="border-2 border-solid rounded-md p-1 hover:text-gray-400">
+      class="border-2 border-solid rounded-md border-white p-1 hover:text-gray-400">
       제품 추가 하기
     </RouterLink>
   </section>
