cf45219f56585210ea630bb2b13ead1620f084b7
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-15 23:44:26 +0900

    feat: buy list component

---
+++ src/routes/user/buyList/BuyList.vue
@@ -0,0 +1,45 @@
+<template>
+  <h2 class="text-4xl p-10">
+    구매 확정 상품
+  </h2>
+  <span v-if="!buyList.length"> ❌ 확정 목록이 없습니다.  </span>
+  <template v-else>
+    <table
+      v-if="buyList.length > 0">
+      <thead>
+        <tr>
+          <th
+            v-for="(head, i) in headName"
+            :key="i">
+            {{ head }}
+          </th>
+        </tr>
+      </thead>
+      <Tbody
+        v-for="list in buyList"
+        :key="list.id"
+        :list="list" />
+    </table>
+  </template>
+</template>
+
+<script>
+import Tbody from './BuyListTable.vue'
+
+export default {
+  components: {
+    Tbody
+  },
+  data(){
+    return {
+      errorConetent: null,
+      headName: ['제품 이름', '제품 가격', '구매 요청 날짜'],
+    }
+  },
+  computed: {
+    buyList(){
+      return this.$store.getters['buyList/buyList']
+    }
+  }
+}
+</script>
