f1a0f0833d1c99815cf7a572916335fd775276e6
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-15 23:43:05 +0900

    feat: sell list tbody part

---
+++ src/routes/admin/sellList/SellListTable.vue
@@ -0,0 +1,45 @@
+<template>
+  <tbody>
+    <tr>
+      <td>{{ list.user.email }}</td>
+      <td>{{ list.user.displayName }}</td>
+      <td>{{ list.product.title }}</td>
+      <td>{{ list.product.price.toLocaleString() + '원' }}</td>
+      <td>{{ list.account.bankName }}</td>
+      <td>{{ list.account.accountNumber }}</td>
+      <td>{{ time }}</td>
+      <template v-if="!list.isCanceled && !list.done">
+        <td class="p-2 bg-green-200">
+          요청
+        </td>
+      </template>
+      <template v-if="list.done">
+        <td class="p-2 bg-blue-200">
+          확정
+        </td>
+      </template>
+      <template v-if="list.isCanceled">
+        <td class="p-2 bg-red-200">
+          취소
+        </td>
+      </template>
+    </tr>
+  </tbody>
+</template>
+
+<script>
+import dayjs from 'dayjs'
+export default {
+  props: {
+    list: {
+      type: Object,
+      default: ()=>({})
+    }
+  },
+  computed: {
+    time(){
+      return dayjs(this.list.timePaid).format('YYYY/MM/DD - HH:mm:ss') 
+    }
+  }
+}
+</script>
