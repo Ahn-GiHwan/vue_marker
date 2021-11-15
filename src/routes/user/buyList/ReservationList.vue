192dbcb776cd1eca4d4460fdd27a9afc7d7b0fa7
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-15 18:58:16 +0900

    feat: reservation goods List component

---
+++ src/routes/user/buyList/ReservationList.vue
@@ -0,0 +1,45 @@
+<template>
+  <h2 class="text-4xl p-10">
+    구매 요청 상품
+  </h2>
+  <span v-if="!reservationList.length"> ❌ 요청 목록이 없습니다.  </span>
+  <template v-else>
+    <table
+      v-if="reservationList.length > 0">
+      <thead>
+        <tr>
+          <th
+            v-for="(head, i) in headName"
+            :key="i">
+            {{ head }}
+          </th>
+        </tr>
+      </thead>
+      <Tbody
+        v-for="list in reservationList"
+        :key="list.id"
+        :list="list" />
+    </table>
+  </template>
+</template>
+
+<script>
+import Tbody from './BuyListTable.vue'
+
+export default {
+  components: {
+    Tbody
+  },
+  data(){
+    return {
+      errorConetent: null,
+      headName: ['제품 이름', '제품 가격', '구매 요청 날짜', '구매확정', '구매 취소'],
+    }
+  },
+  computed:{
+    reservationList(){
+      return this.$store.getters['buyList/reservationList']
+    }
+  },
+}
+</script>
