b1b7011c3a8b4d79096631cd654d5d2318b51493
Ahn-GiHwan <ahngh0113@naver.com>
2021-11-15 18:53:44 +0900

    feat: add store admin, buyList

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,16 @@ import user from './user'
 import account from './account'
 import goods from './goods'
 import alert from './alert'
+import buyList from './buyList'
+import admin from './admin'
 
 export default createStore({
   modules: {
     user,
     account,
     goods,
-    alert
+    alert,
+    buyList,
+    admin
   },
 })
